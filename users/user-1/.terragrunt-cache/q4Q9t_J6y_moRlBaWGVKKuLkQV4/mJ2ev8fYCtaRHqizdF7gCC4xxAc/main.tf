6979258e68ce7ff655a392bf5daeee1a36ba1785
Chandrashekhar Choudha <chandrashekharchoudha@gmail.com>
2025-05-19 16:57:30 +0530

    temp2

--- users/user-1/.terragrunt-cache/q4Q9t_J6y_moRlBaWGVKKuLkQV4/mJ2ev8fYCtaRHqizdF7gCC4xxAc/main.tf
+++
@@ -1,4 +0,0 @@
-resource "google_storage_bucket" "mybucket" {
-  name = var.bucket_name
-  location = var.bucket_location
-}
--- users/user-1/.terragrunt-cache/q4Q9t_J6y_moRlBaWGVKKuLkQV4/mJ2ev8fYCtaRHqizdF7gCC4xxAc/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-bucket_name   = "user-1-bucket"
-bucket_location = "us-central1" 
